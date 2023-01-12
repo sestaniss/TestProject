7d591a7e50cc8efaa8e7262598f70f9f05408759
sestaniss <sam.estaniss@gmail.com>
2023-01-11 23:04:17 -0600

    3rd commit

---
+++ src/homeWork4/Task2.java
@@ -0,0 +1,9 @@
+package homeWork4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 100; i>=1; i--){
+            System.out.print(i+" ");
+        }
+    }
+}
